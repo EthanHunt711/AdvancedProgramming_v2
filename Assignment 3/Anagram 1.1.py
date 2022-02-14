2516cc9682c1bb56ddeea51c02c0a4b400e7051b
EthanHunt711 <79079092+EthanHunt711@users.noreply.github.com>
2022-02-14 23:17:22 +0100

    Progressing

--- Assignment 3/Anagram 1.1.py
+++ Assignment 3/Anagram 1.1.py
@@ -2,28 +2,68 @@ from collections import defaultdict
 from Bubble_sort import bubble_sort
 
 
-anagram_dict = defaultdict(lambda: 'Ordet hittades inte')
+class LexicalEntry:
+    def __init__(self, lex):
+        self.lex = lex
+
+    def split_lex(self):
+        split_list = []
+        for le in self.lex:
+            split_list.extend(le.lower())
+        return split_list
+
+    def joined_sorted_lex(self):
+        s = self.split_lex()
+        j_lex = bubble_sort(s)
+        s_lex = ''.join(j_lex)
+        return s_lex
 
 
-def add_to_dict(filename):  # creating the dictionary
-    with open(filename, encoding='utf8') as in_f:
-        for word in in_f:
-            word = word.strip()
-            anagram_dict[bubble_sort(list(word))] = bubble_sort(list(word))
-
-def find_same_letters(word):
-    for letter in word:
-        pass
-class Anagram:
-    def __init__(self, lexicon):
-        self.lexicon = lexicon
+def word_to_list(word):
+    w_l = []
+    for el in word:
+        w_l.append(el)
+    return ''.join(bubble_sort(w_l))
 
 
-def main():
-    anv_inmat = input('Skriv ett valfritt ord: ')
+def make_dict_keys(opened_file):  # creating the dictionary
+    anagram_dict = defaultdict(list)
+    with open(opened_file, encoding='utf8') as s:
+        for lex_entry_line in s:
+            w = lex_entry_line.strip()
+            u = LexicalEntry(w)
+            anagram_dict[u.joined_sorted_lex()] = []
+    return anagram_dict
 
-    pass
+
+def find_similar_for_values(diction, open_file):
+    with open(open_file, encoding='utf8') as d:
+        for y in d:
+            q = y.strip()
+            j = word_to_list(q)
+            if j in diction.keys():
+                diction[j].append(q)
 
 
-# if __name__ == "__main__":
-#     main()
+def main(filename):
+    q = make_dict_keys(filename)
+    find_similar_for_values(q, filename)
+    return q
+
+
+if __name__ == "__main__":
+    lexi_dict = main('sv-utf8.txt')
+    anv_inm = input('Dags för ett kult ordspel\n\nSkriv in ett valfritt svenskt ord: ')
+    while True:
+        if type(anv_inm) != str:
+            print(f'{anv_inm} är troligtvis ett ogiltigt tecken')
+        else:
+            if word_to_list(anv_inm) not in lexi_dict.keys():
+                print(f'{anv_inm} finns inte i just den här ordlistan')
+            else:
+                if len(lexi_dict[word_to_list(anv_inm)]) == 0:
+                    print(f'Det finns inga anagram för {anv_inm}')
+                else:
+                    print(f'alla ord i den här listan delar samma anagram som ordet inmatade:\n'
+                          f'{lexi_dict[word_to_list(anv_inm)]}')
+
